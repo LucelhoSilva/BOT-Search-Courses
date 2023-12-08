5875014cdcac805bc522a7bb8f3027ca64dcad2e
Lucelho Cristiano Vieira Da Silva <lucelhocristiano28@gmail.com>
2023-12-08 14:33:41 -0300

    Scraping HrBrCursos - Close #4

--- engines/hrbrcursos.py
+++ engines/hrbrcursos.py
@@ -3,13 +3,11 @@ from bs4 import BeautifulSoup
 
 courses = []
 
-
 async def get_hrbrcursos_courses() -> list:
   
   async with httpx.AsyncClient() as client:
     response = await client.get(f'https://www.hrbrcursos.com/cursos/')
     soup = BeautifulSoup(response.text, 'html.parser')
-
     cells = soup.find_all('div', class_="pp-content-grid-inner pp-content-body clearfix")
     for cell in cells :
         title = cell.find('h3').get_text(strip=True)
@@ -17,6 +15,5 @@ async def get_hrbrcursos_courses() -> list:
 
         course = [title, link]
         courses.append(course)
-
 
     return courses
