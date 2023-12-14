a32f2ab9e5ee95b8d5548e27adb47e22b9d2afbc
Lucelho Cristiano Vieira Da Silva <lucelhocristiano28@gmail.com>
2023-12-14 09:52:46 -0300

    Feat: Configurando Keyring

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from engines.cursoemvideo import get_cursoemvideo_courses
 from engines.hrbrcursos import get_hrbrcursos_courses
 
 bot = hikari.GatewayBot(keyring.get_password('bot_cursos', 'token'), intents=intents.Intents.ALL)
-channel_id = 1181318614087897221
+channel_id = keyring.get_password('bot_cursos', 'channel')
 
 sent_courses = []
 
@@ -43,25 +43,25 @@ async def on_started(event: hikari.StartedEvent) -> None:
             await asyncio.sleep(60)
     await asyncio.sleep(60)
 
-    # # HRBR Cursos
-    # results = await get_hrbrcursos_courses()
-    # for result in results:
-    #     if result[0] not in sent_courses:
-    #         sent_courses.append(result[0])
-    #         course_info = f'{"-"*50}\n\nCURSO: {result[0]}\nLINK: {result[1]}'
-    #         await bot.rest.create_message(channel_id, course_info)
-    #         await asyncio.sleep(60)
-    # await asyncio.sleep(60)
+    # HRBR Cursos
+    results = await get_hrbrcursos_courses()
+    for result in results:
+        if result[0] not in sent_courses:
+            sent_courses.append(result[0])
+            course_info = f'{"-"*50}\n\nCURSO: {result[0]}\nLINK: {result[1]}'
+            await bot.rest.create_message(channel_id, course_info)
+            await asyncio.sleep(60)
+    await asyncio.sleep(60)
 
 
-    # # Udemy
-    # results = await get_udemy_courses()
-    # for result in results:
-    #     if result[0] not in sent_courses:
-    #         sent_courses.append(result[0])
-    #         course_info = f'{"-"*50}\n\nCURSO: {result[0]}\nLINK: {result[1]}'
-    #         await bot.rest.create_message(channel_id, course_info)
-    #         await asyncio.sleep(60)
-    # await asyncio.sleep(60)
+    # Udemy
+    results = await get_udemy_courses()
+    for result in results:
+        if result[0] not in sent_courses:
+            sent_courses.append(result[0])
+            course_info = f'{"-"*50}\n\nCURSO: {result[0]}\nLINK: {result[1]}'
+            await bot.rest.create_message(channel_id, course_info)
+            await asyncio.sleep(60)
+    await asyncio.sleep(60)
 
 bot.run()
